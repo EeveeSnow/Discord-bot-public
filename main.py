2e829f027e7bc781af5019690ea27a1d662b7895
Snow Leopard <EeveeSnow@users.noreply.github.com>
2020-11-24 23:37:48 +0300

    version 0.21
    Co-Authored-By: magiczocker10 <59317952+magiczocker10@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,22 +10,21 @@ serverip = "Your server"
 bot = commands.Bot(command_prefix = settings['prefix'])
 nowserver = MinecraftServer.lookup(serverip)
 
-try:
-    status = nowserver.status()
-    serveronline = 1
-except:
-    serveronline = 0
 
-if serveronline == 1 and status != 0:
-    status = nowserver.status()
-    pingnow = status.latency
-    print(status.raw['players'])
-    if len(status.raw['players']) >= 3:
-        usersConnected = [user['name'] for user in status.raw['players']['sample']]
-    else:
-        usersConnected = []
 @bot.command()
 async def Connected(ctx):
+    try:
+        status = nowserver.status()
+        serveronline = 1
+    except:
+        serveronline = 0
+
+    if serveronline == 1 and status != 0:
+        status = nowserver.status()
+        if len(status.raw['players']) >= 3:
+         usersConnected = [user['name'] for user in status.raw['players']['sample']]
+        else:
+         usersConnected = []
     if serveronline == 1:
         embed1 = discord.Embed(color = 0x00e600, title = 'Connected players')
         embed1.add_field(name=f"connected player - {len(usersConnected)}:", value="** **", inline=False)
@@ -38,6 +37,19 @@ async def Connected(ctx):
          await ctx.send(embed = embed1) 
 @bot.command()
 async def full(ctx):
+    try:
+        status = nowserver.status()
+        serveronline = 1
+    except:
+        serveronline = 0
+
+    if serveronline == 1 and status != 0:
+        status = nowserver.status()
+        pingnow = status.latency
+        if len(status.raw['players']) >= 3:
+         usersConnected = [user['name'] for user in status.raw['players']['sample']]
+        else:
+         usersConnected = []
     if serveronline == 1:
         embed2 = discord.Embed(color = 0x00e600, title = 'Server Info')
         embed2.add_field(name="Status: online :white_check_mark:", value="** **", inline=False)
@@ -48,9 +60,9 @@ async def full(ctx):
             embed2.add_field(name=f"`{usersConnected[i]}`", value="** **", inline=True)
         await ctx.send(embed = embed2)
     else:
-         embed1 = discord.Embed(color = 0xff0000, title = 'Server Info')
-         embed1.add_field(name="server now offline:x:", value="** **", inline=False)
-         await ctx.send(embed = embed1)
+         embed2 = discord.Embed(color = 0xff0000, title = 'Server Info')
+         embed2.add_field(name="server now offline:x:", value="** **", inline=False)
+         await ctx.send(embed = embed2)
 @bot.command()
 async def snow(ctx):
     await ctx.send("\u2603\u2603\u2603")
